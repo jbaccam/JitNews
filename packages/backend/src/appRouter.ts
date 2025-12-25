69fcce7341e4275b933da33e6818fbc896b53053
Jeremiah Baccam <jjbaccam@iastate.edu>
2025-12-25 15:33:02 -0600

    remove openstates integration

--- packages/backend/src/appRouter.ts
+++ packages/backend/src/appRouter.ts
@@ -1,14 +1,12 @@
 import { router } from './trpc';
 import { pingRouter } from './routes/ping';
 import { communityRouter } from './routes/community';
-import { openstatesRouter } from './routes/openstates';
 import { newsletterRouter } from './routes/newsletter';
 import { newsRouter } from './routes/news';
 
 export const appRouter = router({
   ping: pingRouter,
   community: communityRouter,
-  openstates: openstatesRouter,
   newsletter: newsletterRouter,
   news: newsRouter,
 });
